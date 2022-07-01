6cce3677c8877a62d60d3b4be7373874dfd6518d
Gers2017 <Gers2017@outlook.com>
2022-07-01 12:15:06 -0600

    Fix delete directory bug
    add ignore_case flag
    use MatchOptions to consume ignore_case flag
    make prompt return bool instead of Result<bool>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::{error::Error, path::{PathBuf}, io, fs};
-use glob::glob;
+use glob::{glob_with, MatchOptions};
 use argh;
 use argh::FromArgs;
 use ansi_term::Colour::{Cyan, Purple, Blue};
 
 #[derive(FromArgs)]
-/// Eradicate files with patterns and no mercy
+/// Eradicate files/directories with patterns and no mercy
 pub struct Eradicate {
     /// the pattern to use for the eradication
     #[argh(positional)]
@@ -15,7 +15,11 @@ pub struct Eradicate {
     #[argh(switch, short = 'i')]
     pub interactive: bool,
 
-    /// forcefully delete files. Don't ask, just do it
+    /// ignore case search
+    #[argh(switch, short = 'g')]
+    pub ignore_case: bool,
+
+    /// forcefully delete entries. Don't ask, just do it
     #[argh(switch, short = 'f')]
     pub force: bool,
 
@@ -28,19 +32,20 @@ type ResultBox<T> = Result<T, Box<dyn Error>>;
 
 fn main() -> ResultBox<()> {
     let erad: Eradicate = argh::from_env();
-    let pattern = erad.pattern;
-    let verbose = erad.verbose;
-    let interactive = erad.interactive;
-    let force = erad.force;
+    let Eradicate { pattern, verbose, interactive, force, ignore_case } = erad;
+    println!("Searching with pattern {}", paint_purple(pattern.to_string()));
+    
+    let options = MatchOptions {
+        case_sensitive: !ignore_case,
+        ..Default::default()
+    };
 
-    println!("Searching files with pattern {}", paint_purple(pattern.to_string()));
-   
-    let paths :Vec<PathBuf> = glob(&pattern)?.filter_map(Result::ok).collect();
+    let paths: Vec<PathBuf> = glob_with(&pattern, options)?.filter_map(Result::ok).collect();
       
     if paths.len() > 0 {
         eradicate(&paths, verbose, interactive, force)?
     } else {
-        println!("No files match the pattern {}", paint_purple(pattern.to_string()));
+        println!("No entries match the pattern {}", paint_purple(pattern.to_string()));
     }
 
     return Ok(());
@@ -48,7 +53,7 @@ fn main() -> ResultBox<()> {
 
 fn eradicate(paths: &Vec<PathBuf>, verbose: bool, interactive: bool, force: bool) -> ResultBox<()> {
     let length = paths.len();
-    println!("Files to eradicate: {}", paint_purple(length.to_string()));
+    println!("Entries to eradicate: {}", paint_purple(length.to_string()));
 
     if verbose {
         let paths_str:Vec<_> = paths.iter().map(|p| p.to_string_lossy()).collect();
@@ -56,52 +61,66 @@ fn eradicate(paths: &Vec<PathBuf>, verbose: bool, interactive: bool, force: bool
     }
 
     if force {
-        delete_files(&paths)?;
+        delete_paths(&paths)?;
         return Ok(());
     }
     
     if interactive {
         for path in paths.iter() {
             let cyan_filepath = paint_cyan(path.display().to_string());
-            let is_delete = prompt(format!("Eradicate {}?", cyan_filepath))?;
+            let is_delete = prompt(format!("Eradicate {}?", cyan_filepath));
             if is_delete {
-                fs::remove_file(path)?;
+                delete_path(path)?;
                 if verbose { println!("{} was eradicated!", cyan_filepath); }
             }
         }
         return Ok(());
     }
 
-    let is_delete = prompt("Eradicate files?".into())?;
+    let is_delete = prompt("Eradicate entries?".into());
     if is_delete {
-        delete_files(&paths)?;
+        delete_paths(&paths)?;
         println!("Eradication completed!");
     } else {
-        println!("No files were eradicated");
+        println!("No entries were eradicated");
     }
     
     Ok(())
 }
 
-fn delete_files(paths: &Vec<PathBuf>) -> io::Result<()> {
+fn delete_paths(paths: &Vec<PathBuf>) -> io::Result<()> {
     for p in paths {
-        fs::remove_file(p)?;
+        delete_path(p)?;
     }
     Ok(())
 }
 
-fn prompt(message: String) -> io::Result<bool> {
+fn delete_path(p: &PathBuf) -> io::Result<()> {
+    if p.is_file() {
+        fs::remove_file(p)?;
+    } else {
+        fs::remove_dir_all(p)?;
+    }
+    Ok(())
+}
+
+fn prompt(message: String) -> bool {
     println!("{} [Y/n]", message);
     let mut buffer = String::new();
     let stdin = io::stdin();
-    stdin.read_line(&mut buffer)?;
+    
+    if let Err(e) = stdin.read_line(&mut buffer) {
+        eprintln!("{}", e);
+        return false;
+    }
+
     let answer = buffer.to_lowercase().replace("\n", "");
     match answer.trim() {
         "y" | "yes" => {
-            Ok(true)
+            true
         },
         _ => {
-            Ok(false)
+            false
         }
     }
 }
